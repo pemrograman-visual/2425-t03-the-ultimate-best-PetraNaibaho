1671aa55dddaaa819d7dac5c40d62195c2477971
PetraNaibaho <petranaibaho5@gmail.com>
2024-10-04 11:14:31 +0700

    Telah di perbaiki

--- T03.java
+++ T03.java
@@ -7,9 +7,9 @@ public class T03 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String iSBN, judul, penulis, penerbit, format, kategori, ket, best;
+        String iSBN, judul, penulis, penerbit, format, kategori, keterangan, ultimate;
         int tahun, stok;
-        double harga, margin, rate, jenisDiskon;
+        double harga, margin, rating, diskon;
 
         iSBN = input.nextLine();
         while (!iSBN.equals("---")) {
@@ -20,32 +20,32 @@ public class T03 {
             format = input.nextLine();
             harga = Double.parseDouble(input.nextLine());
             margin = Double.parseDouble(input.nextLine());
-            jenisDiskon = margin * -1 / harga;
-            if (jenisDiskon > (double) 40 / 100) {
-                ket = "Once in a lifetime";
+            diskon = margin * -1 / harga;
+            if (diskon > (double) 40 / 100) {
+                keterangan = "Once in a lifetime";
             } else {
-                if ((double) 20 / 100 < jenisDiskon && jenisDiskon < (double) 40 / 100) {
-                    ket = "Never come twice";
+                if ((double) 20 / 100 < diskon && diskon < (double) 40 / 100) {
+                    keterangan = "Never come twice";
                 } else {
-                    if (jenisDiskon <= (double) 20 / 100 && jenisDiskon > 0) {
-                        ket = "No regret";
+                    if (diskon <= (double) 20 / 100 && diskon > 0) {
+                        keterangan = "No regret";
                     } else {
-                        ket = "---";
+                        keterangan = "---";
                     }
                 }
             }
             stok = Integer.parseInt(input.nextLine());
-            rate = Double.parseDouble(input.nextLine());
-            if (rate >= 4.7) {
+            rating = Double.parseDouble(input.nextLine());
+            if (rating >= 4.7) {
                 kategori = "Best Pick";
             } else {
-                if (rate >= 4.5 && rate < 4.7) {
+                if (rating >= 4.5 && rating < 4.7) {
                     kategori = "Must Read";
                 } else {
-                    if (rate >= 4.0 && rate < 4.5) {
+                    if (rating >= 4.0 && rating < 4.5) {
                         kategori = "Recommended";
                     } else {
-                        if (rate >= 3.0 && rate < 4.0) {
+                        if (rating >= 3.0 && rating < 4.0) {
                             kategori = "Average";
                         } else {
                             kategori = "Low";
@@ -53,13 +53,17 @@ public class T03 {
                     }
                 }
             }
-            if (kategori.equals("Best Pick") && ket.equals("Once in a lifetime")) {
-                best = "The ultimate best";
+            if (kategori.equals("Best Pick") && keterangan.equals("Once in a lifetime")) {
+                ultimate = "The ultimate best";
             } else {
-                best = "---";
+                ultimate = "---";
             }
-            System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + format + "|" + harga + "|" + margin + "|" + stok + "|" + toxifed(rate, 1) + "|" + kategori + "|" + ket + "|" + best);
+            System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + format + "|" + harga + "|" + margin + "|" + stok + "|" + toFixed(rating,1) + "|" + kategori + "|" + keterangan + "|" + ultimate);
             iSBN = input.nextLine();
         }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
     }
 }
